1ffdcada67085331185c6de2922cddeb448ab355
23001003525-commits <23001003525@jcboseust.ac.in>
2026-02-11 16:58:46 +0530

    Enhance logoutUser cookie settings
    Update cookie options for logout functionality.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,9 @@ const registerUser = asyncHandler(async (req, res) => {
 const logoutUser = (req, res) => {
   res.cookie('jwt', '', {
     httpOnly: true,
-    expires: new Date(0),
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 0
   });
   res.status(200).json({ message: 'Logged out successfully' });
 };
